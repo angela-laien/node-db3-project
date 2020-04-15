dcad7bb0c5dadac9f15ce9dd4477b1c4cbf10664
Laien Xie <angela@Laiens-MacBook-Air.local>
2020-04-15 15:54:06 -0700

    mvp

---
+++ schemes/scheme-model.js
@@ -0,0 +1,43 @@
+const db = require('../data/db-config.js');
+
+module.exports = {
+    find,
+    findById,
+    findSteps,
+    add,
+    // addStep,
+    update,
+    remove
+};
+
+function find() {
+    return db('schemes');
+}
+
+function findById(id) {
+    return db('schemes')
+        .where({ id })
+        .first();
+}
+
+function findSteps(id) {
+    return db.select('*').from('schemes')
+        .join('steps', 'schemes.id', 'steps.scheme_id')
+        .where('schemes.id', id)
+}
+
+function add(scheme) {
+    return db('schemes').insert(scheme);
+}
+
+function update(changes, id) {
+    return db('schemes')
+        .update(changes)
+        .where({ id });
+}
+
+function remove(id) {
+    return db('schemes')
+        .where({ id })
+        .del()
+}
